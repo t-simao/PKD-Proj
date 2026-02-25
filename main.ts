f3a5ff044d2f21780fb7a61747366a6491b8aea8
Talha-Oo <off.bajwatalha@gmail.com>
2026-02-25 16:00:33 +0100

    testing 1

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import { main_menu } from './user_input';
 
-process.stdout.write('\x1Bc'); //CLEARS THE TERMINAL LIKE CONSOLE.CLEAR()
-main_menu();
-process.stdout.write('\x1Bc'); //CLEARS THE TERMINAL LIKE CONSOLE.CLEAR()
+
+async function start(): Promise<void> {
+    process.stdout.write('\x1Bc');
+
+    await main_menu()
+}
+
+start()
